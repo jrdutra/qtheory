5c8ed902fe425b9f63f586ea0184d95a4f5b3d53
jrdutra <jrdutra.com.br@gmail.com>
2020-02-01 21:28:44 -0300

    Params Implemented

--- _multiserver.py
+++ _multiserver.py
@@ -1,5 +1,77 @@
+from math import factorial
 from . import _arrival
 from . import _service
+
+def _eval_Lq(n, c, arr_date_time=[], index_period_beginning=[], start_service=[], end_service=[]):
+
+    roh = _eval_roh(arr_date_time, index_period_beginning, start_service, end_service)
+
+    p0 = _eval_p0(c, arr_date_time, index_period_beginning, start_service, end_service)
+
+    lq = ((roh ** (c + 1))/(factorial(c-1) * ((c-roh) ** 2))) * p0
+
+    return lq
+
+def _eval_pn(n, c, arr_date_time=[], index_period_beginning=[], start_service=[], end_service=[]):
+
+    if c < 1:
+        raise ValueError("The system has to have at last 1 server")
+
+    if n < 0:
+        raise ValueError("The number of clients/items in the queue must be at last 0")
+
+    roh = _eval_roh(arr_date_time, index_period_beginning, start_service, end_service)
+    p0 = _eval_p0(c, arr_date_time, index_period_beginning, start_service, end_service)
+    pn = 0
+
+    if n < c:
+        pn = ((roh ** n) / factorial(n)) * p0
+    
+    else:
+        pn = ((roh ** n) / (factorial(c) * (c ** (n-c)))) * p0
+
+    return pn
+
+def _eval_p0(c, arr_date_time=[], index_period_beginning=[], start_service=[], end_service=[]):
+    
+    if c < 1:
+        raise ValueError("The system has to have at last 1 server")
+
+    roh = _eval_roh(arr_date_time, index_period_beginning, start_service, end_service)
+
+    p0 = 0
+    summ = 0
+
+    if roh/c < 1:
+
+        for n in range(0, c):
+            
+            aux = ((roh ** n) / factorial(n))
+            summ = summ + aux
+
+        term_a = summ
+        term_b = ((roh ** c) / factorial(c))
+        term_c = (1 / (1 - (roh / c)))
+
+        res = term_a + (term_b * term_c)
+
+        p0 = (res ** (-1))
+        
+    
+    else:
+        raise ValueError("roh/c must be lower then 1, but it is " + (roh/c))
+    
+    return p0
+
+def _eval_roh(arr_date_time=[], index_period_beginning=[], start_service=[], end_service=[]):
+
+    mlambda = _arrival._eval_lambda(arr_date_time, index_period_beginning)
+
+    mi = _service._eval_mi(start_service, end_service)
+    
+    roh = mlambda/mi
+
+    return roh
 
 def _eval_lambda_n(n=0, arr_date_time=[], index_period_beginning=[]):
     lambda_n = 0
@@ -17,7 +89,7 @@ def _eval_mi_n(n=0, c=1, start_service=[], end_service=[]):
 
     elif c < 1:
         raise ValueError("c must be at last 1, but c=" + n)
-    
+
     else:
         mi = _service._eval_mi(start_service, end_service)
 
--- multiserver.py
+++ multiserver.py
@@ -1,3 +1,4 @@
+from math import factorial
 from . import _arrival
 from . import _service
 from . import _multiserver
@@ -9,21 +10,139 @@ from . import _multiserver
 # c is the number of servers of the queue system.
 #-------------------------------------------------
 
+def eval_Ws(n, c, arr_date_time=[], index_period_beginning=[], start_service=[], end_service=[]):
+     
+    ls = eval_Ls(n, c, arr_date_time, index_period_beginning, start_service, end_service)
+
+    mlambda = _arrival._eval_lambda(arr_date_time, index_period_beginning)
+
+    ws = ls / mlambda
+
+    return ws
+
+def eval_Wq(n, c, arr_date_time=[], index_period_beginning=[], start_service=[], end_service=[]):
+    lq = _multiserver._eval_Lq(n, c, arr_date_time, index_period_beginning, start_service, end_service)
+
+    mlambda = _arrival._eval_lambda(arr_date_time, index_period_beginning)
+
+    wq = lq / mlambda
+
+    return wq
+
+def eval_Ls(n, c, arr_date_time=[], index_period_beginning=[], start_service=[], end_service=[]):
+
+    lq = _multiserver._eval_Lq(n, c, arr_date_time, index_period_beginning, start_service, end_service)
+
+    roh = _multiserver._eval_roh(arr_date_time, index_period_beginning, start_service, end_service)
+
+    ls = lq + roh
+
+    return ls
+
+def eval_Lq(n, c, arr_date_time=[], index_period_beginning=[], start_service=[], end_service=[]):
+
+    lq = _multiserver._eval_Lq(n, c, arr_date_time, index_period_beginning, start_service, end_service)
+
+    return lq
+
+def eval_pn(n, c, arr_date_time=[], index_period_beginning=[], start_service=[], end_service=[]):
+    """This funtion evaluate the probability do have 0 persons/items in the queue
+
+        Parameters:
+
+        n (int): It is the number of people in the queue. Must be equals or bigger then 0.
+
+        c (int): It is the number of servers in the queue. Must be equals or bigger then 1.
+
+        arr_date_time (str[]): It is the array of datetime. 
+        The format must be: dd-mm-yyyy HH:MM:SS.
+
+        index_period_beginning (int[]): It is the array of beggining positions in the date_time array.
+
+        start_service (str[]): It is the array of datetime meaning the starting of the service.
+        The format must be: dd-mm-yyyy HH:MM:SS.
+
+        end_service (str[]): It is the array of datetime meaning the ending of the service.
+        The format must be: dd-mm-yyyy HH:MM:SS.
+
+        Returns:
+
+        double:roh value
+
+    """
+    pn = _multiserver._eval_pn(n, c, arr_date_time, index_period_beginning, start_service, end_service)
+
+    return pn
+
+def eval_p0(c, arr_date_time=[], index_period_beginning=[], start_service=[], end_service=[]):
+    """This funtion evaluate the probability do have 0 persons/items in the queue
+
+        Parameters:
+
+        c (int): It is the number of servers in the queue. Must be equals or bigger then 1.
+
+        arr_date_time (str[]): It is the array of datetime. 
+        The format must be: dd-mm-yyyy HH:MM:SS.
+
+        index_period_beginning (int[]): It is the array of beggining positions in the date_time array.
+
+        start_service (str[]): It is the array of datetime meaning the starting of the service.
+        The format must be: dd-mm-yyyy HH:MM:SS.
+
+        end_service (str[]): It is the array of datetime meaning the ending of the service.
+        The format must be: dd-mm-yyyy HH:MM:SS.
+
+        Returns:
+
+        double:roh value
+
+    """
+    p0 = _multiserver._eval_p0(c, arr_date_time, index_period_beginning, start_service, end_service)
+
+    return p0
+
+def eval_roh(arr_date_time=[], index_period_beginning=[], start_service=[], end_service=[]):
+
+    """This funtion evaluate the roh = lambda/mi.
+
+        Parameters:
+
+        arr_date_time (str[]): It is the array of datetime. The format must be
+        dd-mm-yyyy HH:MM:SS.
+
+        index_period_beginning (int[]): It is the array of beggining positions in the date_time array.
+
+        start_service (str[]): It is the array of datetime meaning the starting of the service.
+        The format must be: dd-mm-yyyy HH:MM:SS.
+
+        end_service (str[]): It is the array of datetime meaning the ending of the service.
+        The format must be: dd-mm-yyyy HH:MM:SS.
+
+        Returns:
+
+        double:roh value
+
+    """
+    roh = _multiserver._eval_roh(arr_date_time, index_period_beginning, start_service, end_service)
+
+    return roh
+
+
 def eval_lambda_n(n=0, arr_date_time=[], index_period_beginning=[]):
     """This funtion evaluate the lambda n.
 
         Parameters:
 
-            n (int): It is the number of people in the queue. Must be equals or bigger then 0.
+        n (int): It is the number of people in the queue. Must be equals or bigger then 0.
 
-            arr_date_time (str[]): It is the array of datetime. The format must be
-            dd-mm-yyyy HH:MM:SS.
+        arr_date_time (str[]): It is the array of datetime. 
+        The format must be: dd-mm-yyyy HH:MM:SS.
 
-            arr_date_time (int[]): It is the array of beggining positions in the date_time array.
+        index_period_beginning (int[]): It is the array of beggining positions in the date_time array.
 
         Returns:
 
-            double:lambda_n value
+        double:lambda_n value
 
     """
     lambda_n = _multiserver._eval_lambda_n(n, arr_date_time, index_period_beginning)
@@ -35,17 +154,19 @@ def eval_mi_n(n=0, c=1, start_service=[], end_service=[]):
 
         Parameters:
 
-            n (int): It is the number of people in the queue. Must be equals or bigger then 0.
+        n (int): It is the number of people in the queue. Must be equals or bigger then 0.
 
-            start_service (str[]): It is the array of datetime meaning the starting of the service.
-            The format must be dd-mm-yyyy HH:MM:SS.
+        c (int): It is the number of servers in the queue. Must be equals or bigger then 1.
 
-            end_service (str[]): It is the array of datetime meaning the ending of the service.
-            The format must be dd-mm-yyyy HH:MM:SS.
+        start_service (str[]): It is the array of datetime meaning the starting of the service.
+        The format must be: dd-mm-yyyy HH:MM:SS.
+
+        end_service (str[]): It is the array of datetime meaning the ending of the service.
+        The format must be: dd-mm-yyyy HH:MM:SS.
 
         Returns:
-        
-            double: mi_n value
+
+        double: mi_n value
 
     """
     mi_n = _multiserver._eval_mi_n(n, c, start_service, end_service)
